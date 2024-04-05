3f2d5bd8c0a54e9cd12a9e8b3da5e148fcf91e70
Andrey Oliveira <58522443+Andreydso@users.noreply.github.com>
2024-04-05 19:23:30 -0300

    Create Desafio1.cs

---
+++ Desafio1.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static int CalcularDano(int ataque, int defesa)
+    {
+      int dano = ataque - defesa;
+        if (dano < 0) {
+            dano = 0;
+        }
+        return dano;
+    }
+
+    public static void Main (string[] args) {
+
+        int ataque = Convert.ToInt32(Console.ReadLine());
+        int defesa = Convert.ToInt32(Console.ReadLine());
+        int dano = CalcularDano(ataque, defesa);
+        Console.WriteLine($"O dano causado pelo ataque foi: {dano}");
+    }
+
+}
